f81625b7c2aa42db7a237f3b4ec5c25cba944ed4
gabifeo <1309gabi@gmail.com>
2022-02-04 15:13:42 -0300

    Practicando con JS

---
+++ ejerciciosAparte/init.js
@@ -0,0 +1,33 @@
+alert("click alerta");
+
+let nuevoUsuario = prompt ("Ingresa tu nombre");
+    console.log (nuevoUsuario);
+
+console.log("Bienvenidos a la consola💻")
+
+let nombreUsuario = "Gabriela";
+let edadUsuario = 23;
+console.log(nombreUsuario);
+console.log(edadUsuario);
+
+let edadUsuario2 = 21;
+let unionEdad = edadUsuario + edadUsuario2;
+console.log(unionEdad);
+
+let unaMultiplicacion = prompt ("Ingresa un primer número");
+let unaMultiplicacion2 = prompt ("Ingresa un segundo número");
+let resultado = unaMultiplicacion * unaMultiplicacion2;
+    console.log (parseInt (resultado));
+
+let nuevoTexto = prompt ('Escribe en la siguiente línea "Hola Mundo"');
+    if (nuevoTexto === 'Hola Mundo') {
+        console.log('Muy Bien!!');
+    } 
+        else ('Intentalo nuevamente');
+
+let numerosEnteros = prompt ('Escribe un número del 1 al 10');
+    if (numerosEnteros <= 10) {
+        console.log('Excelente!!');
+    } 
+        else ('Era un numero del 1 al 10');
+
